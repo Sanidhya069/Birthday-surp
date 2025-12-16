47eda29425071cc768d03d50585f6f8266e972f6
Sanidhya069 <167228411+Sanidhya069@users.noreply.github.com>
2025-12-16 17:09:00 +0530

    Initial website upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let currentSlide = 0;
+let slides = document.querySelectorAll(".slide, .special");
+
+
+function nextPage() {
+  document.getElementById("page1").classList.add("hidden");
+  document.getElementById("page2").classList.remove("hidden");
+
+  document.getElementById("music").play();
+  startSlider();
+}
+
+function startSlider() {
+  setInterval(() => {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add("active");
+  }, 2500);
+}
+
+function showFinal() {
+  document.getElementById("page2").classList.add("hidden");
+  document.getElementById("page3").classList.remove("hidden");
+}
+function createHeart() {
+  const heart = document.createElement("span");
+  heart.innerHTML = "💖";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = (4 + Math.random() * 3) + "s";
+
+  document.querySelector(".hearts").appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 7000);
+}
+
+setInterval(createHeart, 500);
